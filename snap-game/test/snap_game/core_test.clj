6f0ff925b59dc5ba01184c7812ac453abd94523d
khoovis <khoovirajsingh.seewoonauth@mkodo.com>
2020-07-02 22:03:07 +0100

    decide who won

--- snap-game/test/snap_game/core_test.clj
+++ snap-game/test/snap_game/core_test.clj
@@ -2,7 +2,13 @@
   (:require [clojure.test :refer :all]
             [snap-game.core :refer :all]))
 
+(def player-one {:name "Player One" :think-time 0})
+(def player-two {:name "Player Two" :think-time 1000})
+
 (deftest create-a-deck-of-52-unique-cards
   (is (= 52 (count create-deck)))
   (is (= "Ace of Spade" (first create-deck)))
   (is (= "King of Club" (last create-deck))))
+
+(deftest player-with-quickest-reaction-wins
+  (is (= "Player one won!" (winner?  player-one player-two))))
