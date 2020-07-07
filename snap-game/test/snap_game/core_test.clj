f01e2e444c8fa0b75f0204aac150ea7a46c26260
khoovis <khoovirajsingh.seewoonauth@mkodo.com>
2020-07-07 21:37:30 +0100

    simulate a simple game

--- snap-game/test/snap_game/core_test.clj
+++ snap-game/test/snap_game/core_test.clj
@@ -4,14 +4,21 @@
 
 (def player-one {:name "Player One" :think-time 0})
 (def player-two {:name "Player Two" :think-time 1000})
+(def players [player-one player-two])
+(def ace-of-spade {:rank "A" :suit "S"})
+(def king-of-club {:rank "K" :suit "C"})
+(def deck [ace-of-spade king-of-club])
 
 (deftest create-a-deck-of-52-unique-cards
   (is (= 52 (count create-deck)))
-  (is (= {:rank "A" :suit "S"} (first create-deck)))
-  (is (= {:rank "K" :suit "C"} (last create-deck))))
+  (is (= ace-of-spade (first create-deck)))
+  (is (= king-of-club (last create-deck))))
 
 (deftest player-with-quickest-reaction-wins
   (is (= "Player One" (winner? player-one player-two))))
 
 (deftest snap-when-rank-matches
   (is (= true (snap? {:rank "2" :suit "S"} {:rank "2" :suit "C"}))))
+
+(deftest player-one-wins
+  (is (= "Player One wins!" (simulate-game deck players))))
